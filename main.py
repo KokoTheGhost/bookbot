232e5751e64005ae12c447cba3bfbde321f7c24f
KokoTheGhost <khalfanalshamsi02@gmail.com>
2025-06-30 13:31:59 +0400

    completed the project

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from stats import *
+import sys as sys
 
-def get_book_text():
+def get_book_text(location):
     """
     Reads the content of the book file and returns it as a string.
     """
     try:
-        with open('books/frankenstein.txt', 'r', encoding='utf-8') as file:
+        with open(location, 'r', encoding='utf-8') as file:
             return file.read()
     except FileNotFoundError:
         return "Book file not found."
@@ -16,8 +17,14 @@ def main():
     """
     Main function to execute the book reading functionality.
     """
-    bookPath = 'books/frankenstein.txt'
-    bookText = get_book_text()
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError("Usage: python3 main.py <path_to_book>")
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+    bookPath = sys.argv[1]
+    bookText = get_book_text(bookPath)
     totalWords = get_num_words(bookText)
     charCount = get_num_character(bookText)
     sortedCharCount = sort_dict_by_value(charCount)
